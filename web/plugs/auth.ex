440e76d998a9fb173e569a27e197681481932a5d
Bartosz Nowak <sztosz@gmail.com>
2017-01-19 18:45:49 +0100

    Small readability fixes

--- web/plugs/auth.ex
+++ web/plugs/auth.ex
@@ -38,7 +38,7 @@ defmodule Office.Plugs.Auth do
       user ->
         {:error, :unauthorized, conn}
       true ->
-        dummy_checkpw
+        dummy_checkpw()
         {:error, :not_found, conn}
     end
   end
