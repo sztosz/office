fcc29cabee6ed6b92c23f13a38d847d4d8ef126b
Bartosz Nowak <sztosz.github@gmail.com>
2017-08-31 23:52:47 +0200

    More restyling

--- config/dev.exs
+++ config/dev.exs
@@ -41,3 +41,16 @@ config :office, Office.Repo,
   database: "office_dev",
   hostname: "localhost",
   pool_size: 10
+
+config :ex_debug_toolbar,
+  enable: true
+
+config :office, OfficeWeb.Endpoint,
+  instrumenters: [ExDebugToolbar.Collector.InstrumentationCollector]
+
+config :office, Office.Repo,
+  loggers: [ExDebugToolbar.Collector.EctoCollector, Ecto.LogEntry]
+
+config :phoenix, :template_engines,
+  eex: ExDebugToolbar.Template.EExEngine,
+  exs: ExDebugToolbar.Template.ExsEngine
