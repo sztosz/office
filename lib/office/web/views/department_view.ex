2dfa3ef2799dff2fde6cd83b61d641a5ffbfac86
Bartosz Nowak <sztosz.github@gmail.com>
2018-01-02 20:31:00 +0100

    Hearings and cases formatting

--- lib/office/web/views/department_view.ex
+++ lib/office/web/views/department_view.ex
@@ -1,3 +1,7 @@
 defmodule OfficeWeb.DepartmentView do
   use OfficeWeb, :view
+
+  def human_date_time(date_time) do
+    Enum.join([date_time.day, "/", date_time.month, "/", date_time.year, " ", date_time.hour, ":", date_time.minute])
+  end
 end
